2254f7081e8ce02e0e413842c8d63f0f62da4fb1
fatalmonk <anwar.alvi18@gmail.com>
2026-04-23 01:23:56 +0600

    fix(build): extract vercel build routine into separate script to bypass 256 char limit

---
+++ apps/mobile_app/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "SUPABASE_URL=$SUPABASE_URL" > .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
+echo "MANAGER_EMAIL=$MANAGER_EMAIL" >> .env
+echo "MANAGER_PASSWORD=$MANAGER_PASSWORD" >> .env
+echo "ADMIN_EMAIL=$ADMIN_EMAIL" >> .env
+echo "ADMIN_PASSWORD=$ADMIN_PASSWORD" >> .env
+./flutter/bin/flutter gen-l10n
+./flutter/bin/flutter build web --release
+
