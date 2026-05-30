b2bf69d2934fe15c729222e46f358424a495868e
fatalmonk <anwar.alvi18@gmail.com>
2026-05-30 20:47:43 +0600

    Remove orphaned placeholder files causing analyzer errors

--- apps/mobile_app/lib/features/inventory/presentation/screens/file_picker_stub.dart
+++
@@ -1,9 +0,0 @@
-import 'picked_file_data.dart';
-
-/// Stub file picker for web platform
-/// Throws error since file picking is not supported on web
-Future<PickedFileData> pickCsvFile() async {
-  throw UnsupportedError(
-    'CSV file picking is not supported on web. Please use the mobile app.',
-  );
-}
