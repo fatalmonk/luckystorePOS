752a5290100f9511c98f5c6c7b52ad95d2dad69b
fatalmonk <anwar.alvi18@gmail.com>
2026-06-01 14:52:51 +0600

    fix(mobile): resolve all 46 important & 22 minor bugs, finalize POS stability

--- apps/mobile_app/lib/shared/providers/auth_provider.dart
+++ apps/mobile_app/lib/shared/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../models/app_user.dart';
 
